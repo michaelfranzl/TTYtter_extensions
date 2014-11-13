8495bbc48f6315ad0a00d98e7a018707fdf8fd1b
Michael Franzl <office@michaelfranzl.com>
2014-11-13 20:41:31 +0100

    changed database engine to INNODB, removed charset because it will be taken from the database setting

--- ttytter_mysql/ttytter_mysql_create_tables.sql
+++ ttytter_mysql/ttytter_mysql_create_tables.sql
@@ -1,27 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.20, for Linux (x86_64)
---
--- Host: localhost    Database: pwntter
--- ------------------------------------------------------
--- Server version	5.5.20-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `direct_messages`
 --
 
 DROP TABLE IF EXISTS `direct_messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `direct_messages` (
   `id` bigint(20) unsigned NOT NULL,
   `sender_id` bigint(20) NOT NULL,
@@ -33,16 +14,13 @@ CREATE TABLE `direct_messages` (
   PRIMARY KEY (`id`),
   KEY `sender_id_index` (`sender_id`),
   KEY `recipient_id_index` (`recipient_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Direct Messages';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=INNODB COMMENT='Direct Messages';
 
 --
 -- Table structure for table `tweets`
 --
 
 DROP TABLE IF EXISTS `tweets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tweets` (
   `id` bigint(20) unsigned NOT NULL,
   `user_id` bigint(20) NOT NULL,
@@ -56,16 +34,13 @@ CREATE TABLE `tweets` (
   `geo_long` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id_index` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Tweets';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=INNODB COMMENT='Tweets';
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -88,16 +63,4 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `screen_name_index` (`screen_name`),
   KEY `name_index` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Twitter User Information';
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-02-05  8:35:51
+) ENGINE=INNODB COMMENT='Twitter User Information';
